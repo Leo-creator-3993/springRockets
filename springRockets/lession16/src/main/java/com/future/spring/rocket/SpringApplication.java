e119525c6efd7a0a575b413ab846c700c576cb76
xieyao <xieyao@linklogis.com>
2024-10-08 20:06:44 +0800

    feat(spring):autowire example.

---
+++ springRockets/lession16/src/main/java/com/future/spring/rocket/SpringApplication.java
@@ -0,0 +1,11 @@
+package com.future.spring.rocket;
+
+import org.junit.jupiter.api.Test;
+
+public class SpringApplication {
+
+    @Test
+    public void test0() {
+        System.out.println("hi");
+    }
+}
